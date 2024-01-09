aa0bc15442c8cd98ff0a05411eab68a3705f3129
gandisuryaRC <gandisurya08@gmail.com>
2024-01-09 13:44:46 +0700

    commit

--- _footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="py-3 my-4 bg-dark">
-<p class="text-center text-white">Contact Me</p>
-<center>
-  <a href="https://www.instagram.com/____.____.__.______.____" target="_blank"><img src="picture1/iglogo.png" height="70" class="gambar"></a>
-	<a href="https://www.facebook.com/profile.php?id=100044833560194" target="_blank"><img src="picture1/fblogo.png" height="70" class="gambar"></a>
-  <br><br>
-  <p class="text-center text-muted">&copy;2022</p>
-</center>
-</footer>
--- _js.php
+++
@@ -1,6 +0,0 @@
-<script src="js/bootstrap.min.js"></script>
-<script src="ckeditor/ckeditor.js.map"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/jquery-3.5.1.js"></script>
-<script src="js/jquery.dataTables.min.js"></script>
-<script src="js/dataTables.bootstrap5.min.js"></script>
